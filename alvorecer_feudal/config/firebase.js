7d9a28a2fd279f748849627c173f31ad9ce6d356
wildemberg-sales <junior_sales2010@hotmail.com>
2022-03-17 21:04:34 -0300

    inclusao config firebase

---
+++ alvorecer_feudal/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrMbpldszgGctvO4F3BFDnVHxcquiQstk",
+  authDomain: "dsunb-88ace.firebaseapp.com",
+  projectId: "dsunb-88ace",
+  storageBucket: "dsunb-88ace.appspot.com",
+  messagingSenderId: "800457733727",
+  appId: "1:800457733727:web:2de038947b78a797378dea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
