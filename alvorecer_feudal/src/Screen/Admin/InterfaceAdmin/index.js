1bc8f275e6dbde078c4f8c4702831838a64c0a6a
wildemberg-sales <junior_sales2010@hotmail.com>
2022-04-04 17:22:05 -0300

    pagina menuAdmin concluida

--- alvorecer_feudal/src/Screen/Admin/InterfaceAdmin/index.js
+++ alvorecer_feudal/src/Screen/Admin/InterfaceAdmin/index.js
@@ -1,28 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function InterfaceAdmin({navigation}){
+
+    const [quantCavaleiros, setQuantCavaleiros]= useState('80');
+    const [quantFeudos, setQuantFeudos]= useState('8');
+    const [quantTarefas, setQuantTarefas]= useState('20');
+
+
+
     return(
     <View style = {styles.container}>
-        
-        <TouchableOpacity style={styles.button}
-            onPress={() => navigation.navigate("Construtores")}>
-            <Text>Construtores do Reino</Text>
-        </TouchableOpacity>
+
+        <Text style={styles.greeting}>Olá Monarca :)</Text>
+
+            <View style={styles.containeralfa}>
+
+                <View style={styles.containernumerico}>
+                    <Text style={styles.text}>Quantidade de Cavaleiros</Text>
+                    <Text style={styles.text}>{quantCavaleiros}</Text>
+                </View>
+
+                <View style={styles.containernumerico}>
+                    <Text style={styles.text}>Quantidade de Feudos</Text>
+                    <Text style={styles.text}>{quantFeudos}</Text>
+                </View>
+
+            </View>
+
+            <View style={styles.textcontainer}>
+                <Text style={styles.text}>Existem {quantTarefas} tarefas em aberto</Text>
+                
+            </View>
 
         <TouchableOpacity style={styles.button}
-            onPress={() => navigation.navigate("Feudos")}>
-            <Text>Gerenciar Feudos</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}
-            onPress={() => navigation.navigate("Tarefas")}>
-            <Text>Gerenciar Tarefas</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}
-            onPress={() => navigation.navigate("Ranking")}>
-            <Text>Ranking Feudal</Text>
+            onPress = {() => navigation.navigate('Login')}>
+            <Text style={styles.text}>Sair</Text>
         </TouchableOpacity>
 
     </View>
@@ -38,15 +51,41 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         
     },
-    
-    tittle: {
-        fontSize: 40,
-        color: '#fff',
-        marginBottom: 0,
+
+    containeralfa: {
+        flexDirection: 'row',
+        backgroundColor: '#8425E3',
+        alignItems: 'center',
+        justifyContent: 'center', 
+        
+    },
+
+    textcontainer: {
+        height: 150,
+        width: 320,
+        borderRadius: 20,
+        marginTop: 20,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 15, 
+        
+    },
+
+    containernumerico: {
+        height: 150,
+        width: 150,
+        borderRadius: 20,
+        marginHorizontal: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 15,
+        
     },
 
     button: {
-        height: 100,
+        height: 120,
         width: 320,
         borderRadius: 20,
         marginTop: 20,
@@ -54,6 +93,22 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: '#6EF46B',
         elevation: 15
+
+    },
+
+    text: {
+        margin: 10,
+        fontSize: 20,
+        color: '#484848',
+        alignItems: 'center', 
+        justifyContent: 'center',
+
+    },
+
+    greeting: {
+        margin: 20,
+        fontSize: 30,
+        color: '#fff'
 
     },
 })
