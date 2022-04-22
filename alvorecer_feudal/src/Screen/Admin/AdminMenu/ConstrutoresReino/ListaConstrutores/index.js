5d92de90e486d9abeeada0a4ccb2cf16e9363e6c
João Pedro <jpmorbeck45@gmail.com>
2022-04-22 13:38:32 -0300

    teste array lista

--- alvorecer_feudal/src/Screen/Admin/AdminMenu/ConstrutoresReino/ListaConstrutores/index.js
+++ alvorecer_feudal/src/Screen/Admin/AdminMenu/ConstrutoresReino/ListaConstrutores/index.js
@@ -2,40 +2,47 @@ import { NavigationContainer } from "@react-navigation/native";
 import React, {useState, useEffect} from "react";
 import {View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList, SafeAreaView} from 'react-native';
 import app from '../../../../../../config/firebase.js';
-import {query, collection, getFirestore, onSnapshot} from 'firebase/firestore';
+import {doc, setDoc, query, where, getDocs, collection, getFirestore, onSnapshot} from 'firebase/firestore';
 
 
 export default function GerenciarConstrutores({navigation}){
 
    const db = getFirestore(app)
-
-    var lista = [];
-    useEffect(()=>{  
-        const q = query(collection(db, "Users"));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        lista = [];
-        querySnapshot.forEach((doc) => {
-            lista.push(doc.id);
-        });
-        console.log("users: ", lista.join(", "));
-        });
-    },[]);
+ 
+     var lista = [];
+//   let lista = ['20', '45', '88', '49'];
+//   let lista = [{matricula:'20', name: 'joao'}, {matricula:'45', name: 'jose'}, {matricula:'88', name: 'jamilton'}, {matricula:'49', name: 'jorge'}];
 
     
-    
-
+       const q = query(collection(db, "Users"));
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                lista=[];
+                querySnapshot.forEach((doc) => {
+                lista.push(doc.data());
+        });
+        console.log("Users: ", lista.join(", "));
+        Construtor(doc.data().matricula)
+        });
+   
+        function Construtor({nome}){
+            return(
+             <TouchableOpacity 
+                 style={styles.button}
+             >
+                 <Text style={styles.text}>{nome}</Text>
+             </TouchableOpacity>
+         )}
     return(
             
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             
             <Text style={styles.tittle}>Gerenciar Contrutores</Text>
-
-            <ScrollView contentContainerStyle={styles.scrollContainer}>
-                
-
-                
-            </ScrollView>
-                
+  
+                <FlatList 
+                    data = {lista}
+                    renderItem = {({item}) => <Construtor nome = {item.name}/>}
+                    keyExtractor = {item => item.matricula}
+                />
 
             <TouchableOpacity style={styles.button2}
                 onPress = {() => navigation.navigate('CadastroConstrutor')}
@@ -43,7 +50,7 @@ export default function GerenciarConstrutores({navigation}){
                 <Text style={styles.text}>Adiconar Contrutor</Text>
             </TouchableOpacity>
 
-        </SafeAreaView>
+        </View>
     )
 }
 
